2920c0a6484a9b43ce4d90a7efcae56397825557
Androidmini9x <androidmini92@gmail.com>
2023-11-02 00:55:10 +0300

    Fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 import re
 import logging
 import os
-import mysql.connector
+from mysql.connector.connection import MySQLConnection
 
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
@@ -33,14 +33,17 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to a database"""
-    connector = mysql.connector.connect(
-        user=os.environ.get("PERSONAL_DATA_DB_USERNAME", "root"),
-        password=os.environ.get("PERSONAL_DATA_DB_PASSWORD", ""),
-        host=os.environ.get("PERSONAL_DATA_DB_HOST", "localhost"),
-        database=os.environ.get("PERSONAL_DATA_DB_NAME"))
-    return connector
+def get_db() -> MySQLConnection:
+    '''Returns a connector to the database'''
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', 'holberton')
+    conn = MySQLConnection(user=user,
+                           password=password,
+                           host=host,
+                           database=db_name)
+    return conn
 
 
 class RedactingFormatter(logging.Formatter):
