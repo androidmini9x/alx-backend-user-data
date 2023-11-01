c0aaf850ce25cc058b951754b792949f27b99f5e
Androidmini9x <androidmini92@gmail.com>
2023-11-02 00:21:07 +0300

    Fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 import re
 import logging
 import os
-from mysql.connector import connection
+from mysql.connector.connection import MySQLConnection
 
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
@@ -33,13 +33,13 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> connection.MySQLConnection:
+def get_db() -> MySQLConnection:
     '''Returns a connector to the database'''
     user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
     host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = connection.MySQLConnection(user=user,
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME', '')
+    conn = MySQLConnection(user=user,
                            password=password,
                            host=host,
                            database=db_name)
