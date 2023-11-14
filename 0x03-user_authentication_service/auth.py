c59845fec242aa86d74675d2e6c22ffb65e78a2d
atck3r <atck3r@localdomain>
2023-11-14 21:29:07 +0300

    Solve: 0x03

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -87,6 +87,7 @@ class Auth:
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             hash_password = _hash_password(password)
-            self._db.update_user(user.id, hash_password=hash_password)
+            self._db.update_user(user.id, hash_password=hash_password,
+                                 reset_token=None)
         except NoResultFound:
             raise ValueError
